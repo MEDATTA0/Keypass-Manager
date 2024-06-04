f74b4f19c9cd89cd6608ca98f11478de32b4493b
MEDATTA0 <132127603+MEDATTA0@users.noreply.github.com>
2024-06-04 14:25:40 +0100

    1er commit

--- functions.py
+++ functions.py
@@ -1,8 +1,20 @@
-import mysql.connector
 from cryptography.fernet import Fernet
+import random
+import curses.ascii
+
+def table_searcher(ident_or_site):
+    #Elle recherche la table et le field correspondants
+    #Un identifiant finit pas @..... donc on va chercher dans ident_or_site s'il y a un indice que c'est un compte
+    accounts = ['@gmail.com', '@icloud.com', '@outlook.com']
+    for account in accounts:
+        if account in ident_or_site:
+            return ['keypassManager', 'identifiant']
+        
+    return ['associated_site', 'site']
 
 
 def initialisation():
+    #Tentative de connexion à la base de données
     connexion = mysql.connector.connect(
         host = 'localhost',
         user = 'user1',
@@ -10,33 +22,51 @@ def initialisation():
         database = 'project'
     )
 
+    #Création d'un curseur pour envoyer des requêtes
     cursor = connexion.cursor()
-
     cursor.execute(
         '''CREATE TABLE IF NOT EXISTS keypassManager(
-        site VARCHAR(50) NOT null,
+        compte VARCHAR(50) NOT null,
         identifiant VARCHAR(50) NOT null,
         password VARCHAR(300) NOT null,
         cipher VARCHAR(200))'''
     )
+    #identifiant_id INT AUTO_INCREMENT,
+    #PRIMARY KEY(identifiant_id)
+    #Confirmation de la modification de la table avec commit()
     connexion.commit()
+    
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS associated_site(
+        identifiant VARCHAR(50),
+        site VARCHAR(50)
+        )'''
+        #FOREIGN KEY(identifiant_id) REFERENCES keypassManager(identifiant_id)
+    )
+    #On retourne la connexion si c'est successful
     return connexion
 
-def ajouter(site, identifiant, data, connexion):
+def ajouter(compte, identifiant, data, connexion):
     '''Elle reçoit en paramètre une connexion à la base de donnée,
     un identifiant et un password qu'elle crypte et l'insère dans la base de donnée.
     Elle insère aussi la clé de chiffrement sous forme de valeur hexadécimale
     '''
-    cursor = connexion.cursor()
-
+    #On génère une clé
     key = Fernet.generate_key()
+    #On crée un chiffeur
     cipher = Fernet(key)
+    #On encode le mot de passe pour le switch de string->bytes
     data = data.encode()
+    #On chiffre le mot de passe
     encrypted_data = cipher.encrypt(data)
+    #On crée un curseur à la base de données
+    cursor = connexion.cursor()
+    #On envoie une requête à la base de données
     cursor.execute(
-        '''INSERT INTO keypassManager (site, identifiant, password, cipher)
-        VALUES ('{}', '{}', '{}', '{}')'''.format(site, identifiant, encrypted_data.decode(), key.decode())
+        '''INSERT INTO keypassManager (type, identifiant, password, cipher)
+        VALUES ('{}', '{}', '{}', '{}')'''.format(compte, identifiant, encrypted_data.decode(), key.decode())
     )
+    #On confirme la modification de la table keypassManager
     connexion.commit()
     print("\t\t\tENREGISTREMENT REUSSI\n")
 
@@ -47,6 +77,7 @@ def afficher(connexion):
     cursor.execute(
         '''SELECT * FROM keypassManager'''
     )
+    #On récupère les réponses de la requête dans rows
     rows = cursor.fetchall()
 
     '''Chaque identifiant a son champ où est stocké la clé de chiffrement,
@@ -55,38 +86,126 @@ def afficher(connexion):
     print("Site     Identifiant          Mot de passe\n")
     i = 0
     for row in rows:
+        #On récupère la clé dans row[3] et on crée un chiffreur
         cipher = Fernet(row[3])
+        #On dechiffre le mot de passe avec le cipher créé
         decrypted_data = cipher.decrypt(row[2])
+        #le mot de passe déchiffré est en bytes, on utilise decode() pour le switch de bytes->string
         print("{}       {}        {}".format(row[0], row[1], decrypted_data.decode()))
         i += 1
     print("\t\t{} IDENTIFIANT(S) TROUVES !\n".format(i))
 
-def supprimer(site, connexion):
+
+def supprimer(connexion, ident_or_site):
+    '''Elle supprime en fonction de la table soit un site soit un identifiant'''
     cursor = connexion.cursor()
+    table, field = table_searcher(ident_or_site)
     cursor.execute(
-        '''DELETE FROM keypassManager WHERE site = '{}' '''.format(site)
+    '''DELETE FROM '{}' WHERE '{}' LIKE '%{}%' '''.format(table, field, ident_or_site)
     )
+    
     connexion.commit()
     print("\t\t\tSUPPRESSION REUSSIE !\n")
+        
 
-
-def rechercher(site, connexion):
+def rechercher(connexion, ident_or_site):
+    '''cette fonciton recherche un site ou un identifiant en fonction de la table choisie'''
     cursor = connexion.cursor()
+    table, field = table_searcher(ident_or_site)
     cursor.execute(
-        "SELECT * FROM keypassManager WHERE site = '{}'".format(site)
+       '''SELECT * FROM {} WHERE {} LIKE '%{}%' '''.format(table, field, ident_or_site)
     )
     row = cursor.fetchone()
+    
     return row
 
 
-def modifier(site, new_password, connexion):
-    cursor = connexion.cursor()
-    
+def modifier(connexion, identifiant, new_password):
+    #On génère une clé
     key = Fernet.generate_key()
+    #On crée un cipher avec la clé
     cipher = Fernet(key)
+    #On chiffre le mot de passe, encode permet de switch de string->bytes
     encrypted_password = cipher.encrypt(new_password.encode())
+    #On crée un curseur pour envoyer des requêtes à la base de données
+    cursor = connexion.cursor()
+    #decode permet de switch de bytes->string, afin d'insérer les données sous forme de string et non sous forme de bytes
     cursor.execute(
-        ''' UPDATE keypassManager SET password = '{}', cipher = '{}' WHERE site = '{}' '''.format(encrypted_password.decode(), key.decode(), site)
+        ''' UPDATE keypassManager SET password = '{}', cipher = '{}' WHERE identifiant = '{}' '''.format(encrypted_password.decode(), key.decode(), identifiant)
     )
     connexion.commit()
     print("\n\t\t\t MOT DE PASSE MODIFIE AVEC SUCCES !\n")
+
+
+def ajouter_site_associe(connexion, identifiant):
+    
+    #On crée un curseur pour envoyer des requêtes à la base de données
+    cursor = connexion.cursor()
+    row = rechercher(connexion, identifiant)
+    if row:
+        site = input("Entrer le site associe : ")
+        cursor.execute(
+            '''INSERT INTO associated_site(identifiant, site)
+            VALUES ('{}', '{}')'''
+            .format(identifiant, site)
+            #identifiant_id (a remettre plus tard, cle etrangere)
+        )
+        #On confirme la modification avec commit()
+        connexion.commit()
+        print("\t\t\tSITE ASSOCIE A {} !\n", identifiant)
+    else:
+        print("\t\t\tAUNCUN IDENTIFIANT TROUVE !\n")
+
+
+def supprimer_site_associe(connexion, site=''):
+    #connexion = mysql.connector.connect()
+    cursor = connexion.cursor()
+    cursor.execute(
+        '''DELETE FROM associated_site WHERE site = '{}' '''.format(site)
+    )
+    #On confirme la modification adressée à la base de données grâce à commit
+    connexion.commit()
+    print("\t\t\tSITE SUPPRIME !")
+
+
+def afficher_site_associe(connexion):
+    #connexion = mysql.connector.connect()
+    cursor = connexion.cursor()
+    cursor.execute(
+        '''SELECT * FROM associated_site'''
+    )
+    #On récupère toutes la reponse de requête dans la variable rows 
+    rows = cursor.fetchall()
+    i = 0
+    #On affiche le resultat et on sauvegarde le nombre d'itération dans i
+    print("IDENTIFIANT                    SITE")
+    for row in rows:
+        print("{}      {}".format(row[0], row[1]))
+        i += 1
+
+    print("\t\t\t{} SITE(S) TROUVE(S) !\n".format(i))
+
+def password_gen():
+    nb_caracteres = int(input("Entrer le nombre de caracteres : "))
+    satisfait = False
+    checker = True
+    #Generer des mots de passe en boucle
+    while checker :
+        password =""
+        i = 0
+        #Generer un mot de passe avec des alphanumeriques jusqu'au nombre de caracteres desire
+        while i <= nb_caracteres:
+            letter = chr(random.randint(0, 255))
+            if curses.ascii.isalnum(letter):
+                i += 1
+                password = password+letter
+        #password = ord(random.randbytes(nb_caracteres))
+        print(password)
+        checker = bool(int(input("\n\t\tReessayer un autre ? (0/1) : ")))
+    
+    #On l'enregistre si l'utilisateur est satisfait
+    satisfait = bool(int(input("\t\t\tVoulez-vous l'enregistrer ? (0/1) : ")))
+    if satisfait and not checker:
+        return password
+    else:
+        return ""
--- main.py
+++ main.py
@@ -1,45 +1,103 @@
 import mysql.connector
 from cryptography.fernet import Fernet
 import functions
+import curses.ascii
+import string
 #Menu
+
+conn = functions.initialisation()
 print("\t\t\t Bienvenue Keypass Manager\n")
 checker = True
+
 while checker :
         
     print("\t\t\t\tMenu\n")
-    print(" 1. Ajouter un identifiant\n 2. Supprimer un identifiant\n 3. Modifier le mot de passe d'un identifiant\n 4.Afficher tous les identifiants\n 5. Rechercher un identifiant\n 6. Quiter")
+    print(''' 1. Ajouter un identifiant\n 2. Supprimer un identifiant\n 3. Modifier le mot de passe d'un identifiant 
+ 4.Afficher tous les identifiants\n 5. Rechercher un identifiant\n\n 6. Ajouter un site
+ 7.Supprimer un site\n 8. Afficher les sites\n 9. Rechercher un site\n\n 10. Quitter\n''')
+    choice = input("Entrer votre choix : ")
     try:
-        choice = int(input("Entrer votre choix : "))
-        conn = functions.initialisation()
-
-    except ValueError as error:
-        print("\t\t\t\tVous devrer entrer un entier !\n ", error)
-    #except ConnectionError as err:
-    #    print("\t\t\t\tErreur de connexion à la base de données", err)
-
+        choice = int(choice)
+    except ValueError as err:
+        print("Veuillez entrer un nombre !")
+        choice = '_'
+    
+    if choice not in range(1, 11):
+        choice = '_'
+    
     match choice:
         case 1:
-            site = input("Site : ")
-            id = input("Identifiant : ")
-            password = input("Mot de passe : ")
-            functions.ajouter(site, id, password, conn)
+            password = ""
+            compte = input("Type de compte : ")
+            identifiant = input("Identifiant : ")
+            suggestion = int(input("Voulez-vous une suggestion de mot de passe ? (0/1)"))
+            if suggestion:
+                password = functions.password_gen()
+            elif password == "":
+                password = input("Entrer votre mot de passe : ")
+            functions.ajouter(compte, identifiant, password, conn)
+            break
+
         case 2:
-            site = input("Entrer le site de l'identifiant : ")
-            functions.supprimer(site, conn)
+            
+            identifiant = input("Entrer l'identifiant : ")
+            functions.supprimer(conn, identifiant)
+            break
+
         case 3:
-            site = input("Entrer le site : ")
-            new = input("Nouveau Mot de passe : ")
-            functions.modifier(site, new, conn)
+            identifiant = input("Entrer l'identifiant : ")
+            new_password = input("Nouveau Mot de passe : ")
+            functions.modifier(conn, identifiant, new_password)
+            break
+
         case 4:
             functions.afficher(conn)
+            break
+
         case 5:
-            site = input("Enter le site : ")
-            row = functions.rechercher(site, conn)
+            site_or_account = input("Enter le site/identifiant : ")
+            row = functions.rechercher(conn, site_or_account)
             cipher = Fernet(row[3])
             decrypted_data = cipher.decrypt(row[2])
-            print("Site        Identifiant          Mot de passe\n")
+            print("Type        Identifiant          Mot de passe\n")
             print("{}       {}      {}".format(row[0], row[1], decrypted_data.decode()))
+            break
+
         case 6:
+            identifiant = input("Entrer un identifiant : ")
+            functions.ajouter_site_associe(conn, identifiant)
+            break
+
+        case 7:
+            site = input("Entrer le nom du site : ")
+            functions.supprimer_site_associe(conn, site)
+            break
+
+        case 8:
+            functions.afficher_site_associe(conn)
+            break
+
+        case 9:
+            site = input("Entrer le nom du site : ")
+            row = functions.rechercher(conn, site)
+            print("Identifiant              Site\n")
+            print("{}        {}".format(row[0], row[1]))
+            break
+
+        case 10:
             checker = False
+            print("\t\t\tBye !!!")
+            break
+
+        case 11:
+            nb = int(input("Entrer le nombre de caracteres : "))
+            functions.password_gen(nb)
+            break
+        
+        case '_':
+            break
+    
+    print("\n")
+'''Ajouter, supprimer, afficher les sites associe a un compte'''
         
 conn.close()
